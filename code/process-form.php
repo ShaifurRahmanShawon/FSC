2bb02ace73f1d73d83e25000f40af40b148587bf
ShaifurRahmanShawon <shaifur999@gmail.com>
2023-11-19 16:58:44 +0600

    form update

---
+++ code/process-form.php
@@ -0,0 +1,27 @@
+<?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $name = $_POST["name"];
+        $email = $_POST["email"];
+        $subject = $_POST["subject"];
+        $message = $_POST["message"];
+    
+        // Set the recipient email address
+        $to = "shaifur999@gmail.com";
+    
+        // Build the email message
+        $email_message = "Name: $name\n";
+        $email_message .= "Email: $email\n";
+        $email_message .= "Subject: $subject\n";
+        $email_message .= "Message:\n$message";
+    
+        // Set additional headers
+        $headers = "From: $email";
+    
+        // Send the email
+        mail($to, $subject, $email_message, $headers);
+    
+        // Redirect to a thank you page or display a success message
+        header("Location: thank-you.html");
+        exit();
+    }
+    ?>
